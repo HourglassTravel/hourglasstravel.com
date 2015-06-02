a64f001e10ae42fd5ac8c26925f094ad756201ff
garrettjoecox <garrettjcox@gmail.com>
2015-06-02 00:20:14 -0500

    (fix) Update port for production

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
-
+var port = process.env.PORT || 9000;
 var app = express();
 
 app.use(express.static(__dirname + '/client'));
-app.listen(9000);
+app.listen(port);
