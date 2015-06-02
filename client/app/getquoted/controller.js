1d9f6a5f5c3155b58af18f57acf4bd77c36070e8
garrettjoecox <garrettjcox@gmail.com>
2015-06-02 03:42:41 -0500

    (add) main angular view/controllers & rough quote view

---
+++ client/app/getquoted/controller.js
@@ -0,0 +1,20 @@
+angular.module('hourglasstravel')
+  .controller('getquoted', controller);
+
+function controller($http) {
+
+  this.getLocation = function(val) {
+    return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {
+      params: {
+        address: val,
+        sensor: false
+      }
+    }).then(function(response){
+      return response.data.results.map(function(item){
+        return item.formatted_address;
+      });
+    });
+  };
+
+
+}
